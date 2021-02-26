f756749243bfa0deefb8b1d07cee4c2983ee7a23
ImportantCoding <darrylrhardin@gmail.com>
2021-02-26 15:04:13 -0500

    initial migration tables

--- database/migrations/2021_02_23_204846_create_brand_user_table.php
+++ database/migrations/2021_02_23_204846_create_brand_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSelectedBrandsTable extends Migration
+class CreateBrandUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,10 @@ class CreateSelectedBrandsTable extends Migration
      */
     public function up()
     {
-        Schema::create('selected_brands', function (Blueprint $table) {
+        Schema::create('brand_user', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users');
-            $table->unsignedBigInteger('brand_id');
-            $table->foreign('brand_id')->references('id')->on('brands');
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('brand_id')->constrained();
             $table->timestamps();
         });
     }
@@ -30,6 +28,6 @@ class CreateSelectedBrandsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('selected_brands');
+        Schema::dropIfExists('brand_user');
     }
 }
