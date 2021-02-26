c459b1f98ce3ad49b6f3548f1e7306b70baa6ccf
Darryl <36238285+importantcoding@users.noreply.github.com>
2021-02-26 08:58:56 -0500

    Update 2021_02_23_211709_create_order_statuses_table.php
    name to status_name

--- database/migrations/2021_02_23_211709_create_order_statuses_table.php
+++ database/migrations/2021_02_23_211709_create_order_statuses_table.php
@@ -17,7 +17,7 @@ class CreateOrderStatusesTable extends Migration
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
-            $table->string('name');
+            $table->string('status_name');
             $table->integer('position');
             $table->timestamps();
         });
