9d7c8962c05747a6012e4056f312edc76cb06fe0
ImportantCoding <darrylrhardin@gmail.com>
2021-02-26 22:09:56 -0500

    added phone length in column for customers and made category_ids nullable

--- database/migrations/2014_10_13_000001_create_users_table.php
+++ database/migrations/2014_10_13_000001_create_users_table.php
@@ -23,7 +23,7 @@ class CreateUsersTable extends Migration
             $table->string('logo_path')->nullable();
             $table->boolean('godmode')->default(false);
             $table->foreignId('package_id')->constrained();
-            $table->string('category_ids'); // list of all brand category IDs a business uses (Shoes, Tools, Sports, etc) from the categories table
+            $table->string('category_ids')->nullable(); // list of all brand category IDs a business uses (Shoes, Tools, Sports, etc) from the categories table
             $table->timestamps();
         });
     }
